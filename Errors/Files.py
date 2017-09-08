60ba3d8e29d2188804bfcab4f4d2b9c59d223fac
Aitor Calero <aitor.calero@esri.es>
2017-09-08 08:53:22 +0200

    Sentiment analysis example

--- Errors/Files.py
+++ Errors/Files.py
@@ -1,6 +1,10 @@
-DATAPATH = r'/Users/aitorcalero/Downloads/' \
-           r'0636920003434-master-1416388d72726c5f852316e085111af06da60249' \
-           r'/code/chapter3/sketch.txt'
+import os
+import pickle
+
+DATAPATH = r'sketch.txt'
+
+man = []
+other = []
 
 try:
     data = open(DATAPATH)
@@ -8,11 +12,21 @@ try:
     for eachline in data:
         try:
             (role,line_spoken) = eachline.split(":",maxsplit=1)
-            print(role,end='')
-            print(' said: ', end='')
-            print(line_spoken, end='')
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            elif role == 'Other Man':
+                other.append(line_spoken)
         except ValueError:
             pass
     data.close()
-except IOError:
-    print('the datafile is missing or something wrong has happened ')
+except IOError as err:
+    print('the datafile is missing or something wrong has happened: '+str(err))
+
+try:
+    with open('man_data.txt','wb') as man_data:
+        pickle.dump(man,man_data)
+    with open('other_data.txt','wb') as other_data:
+        pickle.dump(other,other_data)
+except pickle.PickleError as perr:
+    print('Pickling error: '+ str(perr))
