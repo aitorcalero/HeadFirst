1c289ae45a3107ae36308072bcdbcb768a8bf7f7
Aitor Calero <aitor.calero@esri.es>
2017-09-15 20:10:39 +0200

    Examples about classes and dictionaries

--- Data/data.py
+++ Data/data.py
@@ -10,25 +10,54 @@ def sanitize(time_string):
     (mins, secs) = time_string.split(splitter)
     return (mins + '.' + secs)
 
+
+class Athlete:
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+
+    def top3(self):
+        return (sorted(set([sanitize(t) for t in self.times]))[0:3])
+
+    def add_time(self,new_time):
+        self.times.append(new_time)
+
+    def add_times(self,new_list_times):
+        self.times.extend(new_list_times)
+
+class AthleteList(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    def top3(self):
+        return (sorted(set([sanitize(t) for t in self]))[0:3])
+
+
 def open_files(file_name):
     try:
         with open(file_name) as file:
             data = file.readline()
 
-        return data.strip().split(',')
+        templ = data.strip().split(',')
+        return (AthleteList(templ.pop(0), templ.pop(0), templ))
     except IOError as err:
-        print('Error: '+str(err))
+        print('Error: ' + str(err))
         return None
 
 
-james = open_files('james.txt')
+james = open_files('james2.txt')
 julie = open_files('julie.txt')
 mikey = open_files('mikey.txt')
 sarah = open_files('sarah.txt')
 
+print(james.name + "'s fastest times are: " + str(james.top3()))
 
-print(sorted(set([sanitize(each_t) for each_t in james]))[0:3])
-print(sorted(set([sanitize(each_t) for each_t in julie]))[0:3])
-print(sorted(set([sanitize(each_t) for each_t in mikey]))[0:3])
-print(sorted(set([sanitize(each_t) for each_t in sarah]))[0:3])
+james.append('0.67')
+list = ['0.68','0-34']
+james.extend(list)
 
+print(james.top3())
